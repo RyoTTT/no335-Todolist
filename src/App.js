55468299966be68e1356f6de44fb46429ff90718
RTTT <rttt98520@gmail.com>
2022-12-12 16:58:23 +0900

    set html and css

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
 import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+<div className="submit">
+  <input type="text" placeholder='todoを入力'></input>
+  <button>追加</button>
+</div>
+<p>「TODO」</p>
+<div className='todolist'>
+  <ul>
+    <li>todo1</li>
+    <button>完了</button>
+    <button>削除</button>
+  </ul>
+</div>
+<p>「完了済TODO」</p>
+<div className="compTodo">
+  <ul>
+    <li>CompTodo</li>
+    <button>完了</button>
+  </ul>
+</div>
+</>
   );
 }
 
